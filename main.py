adc8883465f23318416078e37cccaaa52e4c09b8
vyash0048-bit <vyash0048@gmail.com>
2026-07-17 18:48:08 +0530

    feat: Implement data ingestion configuration and pipeline
    - Added ConfigurationManager to manage configuration loading and directory creation.
    - Introduced DataIngestionConfig dataclass for structured data ingestion configuration.
    - Created DataIngestion class to handle downloading and extracting data from Google Drive.
    - Developed stage_01_data_ingestion pipeline to orchestrate data ingestion process.
    - Added Jupyter notebook for testing data ingestion functionality.
    - Defined constants for configuration file paths.

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Starting the CNN Classifier application...")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed!<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
