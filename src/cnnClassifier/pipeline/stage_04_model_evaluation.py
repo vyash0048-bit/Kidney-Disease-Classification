c5f275d0c4c82f33121c19b5e58add7ca299630d
vyash0048-bit <vyash0048@gmail.com>
2026-07-20 16:46:15 +0530

    fix: Update GitHub Actions to use latest action versions for improved stability

--- src/cnnClassifier/pipeline/stage_04_model_evaluation.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation.py
@@ -16,7 +16,7 @@ class EvaluationPipeline:
         evaluation = Evaluation(config=eval_config)
         evaluation.evaluation()
         evaluation.save_score()
-        evaluation.log_into_mlflow()
+        # evaluation.log_into_mlflow()
 
 
 if __name__ == "__main__":
